f6ec05afa39d9663fd0315a71d8f3489844436d3
Crowdbotics <team@crowdbotics.com>
2023-02-02 17:43:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soft-wave-38665",
+  url: "https://soft-wave-38665.botics.co",
+  api: "https://soft-wave-38665.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
